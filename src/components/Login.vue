90fbceca7e576eb3a941a9e806b938084d9d58f7
Coco <997651877@qq.com>
2018-12-20 22:01:46 +0800

    登录功能完成

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,47 +1,81 @@
 <template>
   <div class="login">
-  <el-form ref="form" :model="form" :rules="rules" label-width="80px">
-    <img src="../assets/avatar.jpg" alt="">
-    <el-form-item label="用户名" prop="username">
-      <el-input v-model="form.username"></el-input>
-    </el-form-item>
-    <el-form-item label="密码" prop="password">
-      <el-input v-model="form.password"></el-input>
-    </el-form-item>
-    <el-form-item>
-      <el-button type="warning" @click="login">登录</el-button>
-      <el-button id="cancel">重置</el-button>
-    </el-form-item>
-  </el-form>
+    <el-form status-icon ref="form" :model="form" :rules="rules" label-width="80px">
+      <img src="../assets/avatar.jpg" alt>
+      <el-form-item label="用户名" prop="username">
+        <el-input v-model="form.username"></el-input>
+      </el-form-item>
+      <el-form-item label="密码" prop="password">
+        <el-input v-model="form.password"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="warning" @click="login">登录</el-button>
+        <el-button id="cancel" @click="reset">重置</el-button>
+      </el-form-item>
+    </el-form>
   </div>
 </template>
 
 <script>
+// 导入axios
+import axios from 'axios'
 export default {
-  data () {
+  data() {
     return {
+      // 收集表单数据
       form: {
         username: '',
         password: ''
       },
+      // 表单校验
       rules: {
         username: [
-          {required: true, message: '请输入用户名称', trigger: 'blur'},
-          {min: 3, max: 6, message: '长度在 3 到 6 个字符', trigger: 'blur'}
+          { required: true, message: '请输入用户名称', trigger: 'change' },
+          { min: 3, max: 9, message: '长度在 3 到 9 个字符', trigger: 'change' }
         ],
         password: [
-          {required: true, message: '请输入用户密码', trigger: 'blur'},
-          {min: 6, max: 12, message: '长度在 6 到 12 个字符', trigger: 'blur'}
+          { required: true, message: '请输入用户密码', trigger: 'change' },
+          {
+            min: 6,
+            max: 12,
+            message: '长度在 6 到 12 个字符',
+            trigger: 'change'
+          }
         ]
       }
     }
   },
   methods: {
+    reset() {
+      // 获取表单组件
+      this.$refs.form.resetFields()
+    },
     // 登录方法
-    login () {
+    login() {
       this.$refs.form.validate(valid => {
         if (valid) {
-          alert('登录成功')
+          // 发送ajax请求 进行登录
+          axios({
+            method: 'post',
+            url: 'http://localhost:8888/api/private/v1/login',
+            data: this.form
+          }).then(res => {
+            if (res.data.meta.status === 200) {
+              // alert('登录成功')
+              this.$message.success('登录成功')
+              // 把后台颁发的token存起来
+              localStorage.setItem('token', res.data.data.token)
+              // 跳转到home组件
+              // 参数跳转的路径
+              this.$router.push('/home')
+            } else {
+              this.$message({
+                message: res.data.meta.msg,
+                type: 'error',
+                duration: 1000
+              })
+            }
+          })
         } else {
           // 校验失败
           return false
@@ -65,6 +99,13 @@ export default {
     width: 400px;
     margin: 200px auto;
     position: relative;
+    .el-form-item__label {
+      text-align: center;
+    }
+    .el-form-item.is-required:not(.is-no-asterisk)
+      > .el-form-item__label:before {
+      content: '';
+    }
     #cancel {
       margin-left: 175px;
     }
--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,12 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Login from 'components/Login'
+// 导入home
+import Home from 'components/Home'
 
 Vue.use(Router)
 
-export default new Router({
+const router = new Router({
   routes: [
     {
       path: '/',
@@ -13,6 +15,31 @@ export default new Router({
     {
       path: '/login',
       component: Login
+    },
+    {
+      path: '/home',
+      component: Home
     }
   ]
 })
+
+// 给router对象注册一个导航守卫
+// 将来所有的导航都要经过beforeeach
+// next是否方向 next():表示直接放行 next('/login')跳转到
+router.beforeEach((to, from, next) => {
+  // 去登录页直接放行
+  if (to.path === '/login') {
+    // 直接放行
+    next()
+    return
+  }
+  // 如果不是登录，判断是否有token，如果有放行，如果没有去登录
+  let token = localStorage.getItem('token')
+  if (token) {
+    next()
+  } else {
+    next('/login')
+  }
+})
+
+export default router
